c226b6dc999049418c90453a0fa88a03c9929a18
Muja98 <stefan.muja@elfak.rs>
2021-06-15 19:31:49 +0200

    little changes

--- SOA/SOA_PROJEKAT_1/SOA_PROJEKAT_1/WebDashboard/src/app/components/home/home.component.ts
+++ SOA/SOA_PROJEKAT_1/SOA_PROJEKAT_1/WebDashboard/src/app/components/home/home.component.ts
@@ -12,8 +12,51 @@ export class HomeComponent implements OnInit {
     config.keyboard = false;
    }
 
+  public disableSelect:boolean = false;
+  public valueFromDataSelect:string = "";
+
+  numberOfTodos:number = 0;
+  nubmerOfTodoItems:number = 0;
+  numberOfFinishedItems:number=0;
+  searchDate:string = "";
+  itemsPerPage:number = 5;
+  itemsCount:number = 0;
+  page:number = 1;
+  from = 0;
+  to = this.itemsPerPage;
+  todoHabbit = [];
+ 
+
   ngOnInit(): void {
 
+  }
+
+  handleGetDataBy(valueFromDataSelect: string):void
+  {
+    if(valueFromDataSelect === "1")
+      this.disableSelect = false;
+    else
+      this.disableSelect = true;
+
+    this.valueFromDataSelect = valueFromDataSelect;
+  }
+
+  handlePageNumber=(pageNumber)=>{
+    
+    if(!pageNumber || pageNumber===undefined || pageNumber === null) {
+        this.from = 0;
+        this.to = this.itemsPerPage;
+    } else {
+        if(pageNumber === Math.floor(this.itemsCount / this.itemsPerPage) + 1) {
+          this.from = (pageNumber-1)*this.itemsPerPage;
+          this.to = this.from+(this.itemsCount-this.from);
+           
+
+        } else {
+          this.from = (pageNumber - 1) * this.itemsPerPage;
+          this.to = this.from + this.itemsPerPage;
+        }
+    }
   }
 
   open(content) {
